ab45cf64f14ae90967b52790b02299e14a33a04d
Vincent Demeester <vincent@sbr.pm>
2018-08-04 14:56:44 +0200

    Update default.nix (simplified)
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- default.nix
+++ default.nix
@@ -1,24 +1,14 @@
-let
-        _pkgs = import <nixpkgs> {};
-in
-        { pkgs ? import (_pkgs.fetchFromGitHub {
-                owner = "NixOS";
-                repo = "nixpkgs-channels";
-                rev = "9c048f4fb66adc33c6b379f2edefcb615fd53de6";
-                sha256 = "18xbnfzj753bphzmgp74rn9is4n5ir4mvb4gp9lgpqrbfyy5dl2j";
-        }) {}
-}:
 
-pkgs.stdenv.mkDerivation rec {
-        name = "go-projects";
-        env = pkgs.buildEnv { name = name; paths = buildInputs; };
-        buildInputs = [
-                pkgs.go_1_9
-                pkgs.vndr
-                pkgs.gnumake
-                pkgs.gotools
-                pkgs.golint
-                pkgs.godef
-                pkgs.gocode
-        ];
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "ape";
+  buildInputs = [
+    pkgs.go_1_10
+    pkgs.vndr
+    pkgs.gnumake
+    pkgs.gotools
+    pkgs.golint
+    pkgs.godef
+    pkgs.gocode
+  ];
 }
